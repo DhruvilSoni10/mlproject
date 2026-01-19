dc606788316997785ae963c9e16436aacf5f2bdc
Dhruvil J Soni <dhsoni1010@gmail.com>
2026-01-19 21:22:17 +0530

    new commit

--- src/exception.py
+++ src/exception.py
@@ -5,8 +5,8 @@ import logging
 def error_message_detail(error, error_detail: sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format()
-    file_name, exc_tb.tb_lineno, str(error)
+    error_message="Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+    file_name, exc_tb.tb_lineno, str(error))
     return error_message
     
     
@@ -14,7 +14,7 @@ def error_message_detail(error, error_detail: sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
 
 
@@ -26,6 +26,6 @@ if __name__ == "__main__":
     try:
         a = 1 / 0
     except Exception as e:
-        logging.info("Logging has started.")
-        raise CustomException
+        logging.info("Divide By Zero Exception occurred")
+        raise CustomException(e, sys)
     
